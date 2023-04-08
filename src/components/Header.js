730cc32500c9d9a889b3d9f65dca769fee90a79d
Ferdinand BLANCHART <ferdinand_m20@yahoo.fr>
2023-04-08 20:40:59 +0200

    nom du logo header changé

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Logo from "../assets/logo-header.svg";
+import Logo from "../assets/logo_header.svg";
 
 const header = () => {
   return (
