27f67b14ae3a2bc0d94f53eadad81337b5ba97d1
Ferdinand BLANCHART <ferdinand_m20@yahoo.fr>
2023-04-08 15:42:59 +0200

    Ajout du fichier Routes.js où on place tous nos routes dans un fichier séparé

---
+++ src/Routes.js
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Apartment from "./pages/Apartment";
+import NotFound from "./pages/NotFound";
+
+const myRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/apartment/:id" element={<Apartment />} />
+      <Route path="/about" element={<About />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
+
+export default myRoutes;
