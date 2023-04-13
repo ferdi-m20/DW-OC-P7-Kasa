e2ed7e379f5561e13cd81f12dadc153d228a2214
Ferdinand BLANCHART <ferdinand_m20@yahoo.fr>
2023-04-14 01:00:46 +0200

    Page NotFound crée en ajoutant les composants nécessaires et ajout de la page NotFound dans notre Router.js

--- src/Routes.js
+++ src/Routes.js
@@ -2,15 +2,15 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 // import About from "./pages/About";
 // import Apartment from "./pages/Apartment";
-// import NotFound from "./pages/NotFound";
+import NotFound from "./pages/NotFound";
 
 const myRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       {/* <Route path="/apartment/:id" element={<Apartment />} />
-      <Route path="/about" element={<About />} />
-      <Route path="*" element={<NotFound />} /> */}
+      <Route path="/about" element={<About />} /> */}
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
---
+++ src/pages/NotFound.js
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+function NotFound() {
+  return (
+    <div className="not-found">
+      <Header />
+      <div className="not-found-info">
+        <h1 className="not-found-title">404</h1>
+        <p className="not-found-paragraph">
+          Oups! La page que vous demandez n'existe pas.
+        </p>
+        <Link className="not-found-link" to="/">
+          Retourner sur la page d'accueil
+        </Link>
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default NotFound;
