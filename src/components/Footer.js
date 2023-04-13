5776040c8b41186c91f2c58e1b29c5b798ffb509
Ferdinand BLANCHART <ferdinand_m20@yahoo.fr>
2023-04-13 16:48:01 +0200

    Composant Footer crée

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,14 @@
+import logoFooter from "../assets/logo_footer.svg";
+
 const footer = () => {
-  return;
+  return (
+    <div className="footer">
+      <footer>
+        <img src={logoFooter} alt="Kasa, location d'appartement" />
+        <p className="copyright">© 2020 Kasa. All rights reserved</p>
+      </footer>
+    </div>
+  );
 };
 
 export default footer;
