1dbbf7850cb8840d70252b587dab426450f371f3
Ferdinand BLANCHART <ferdinand_m20@yahoo.fr>
2023-04-13 20:41:32 +0200

    Style ajouté pour le composant Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,12 @@
 import logoFooter from "../assets/logo_footer.svg";
+import "../styles/Footer.css";
 
 function Footer() {
   return (
     <div className="footer">
       <footer>
         <img src={logoFooter} alt="Kasa, location d'appartement" />
-        <p className="copyright">© 2020 Kasa. All rights reserved</p>
+        <p className="footer-paragraph">© 2020 Kasa. All rights reserved</p>
       </footer>
     </div>
   );
