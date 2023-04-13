71597fcb8e18883cbe06be9661a6aae04b0f222f
Ferdinand BLANCHART <ferdinand_m20@yahoo.fr>
2023-04-14 01:08:56 +0200

    Style ajouté pour la page NotFound.js

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import "../styles/NotFound.css";
 
 function NotFound() {
   return (
