28ad8ae7d7c6984fc2e61cf38209e7827036cf78
Ferdinand BLANCHART <ferdinand_m20@yahoo.fr>
2023-04-15 00:02:55 +0200

    Composant Gallery.js configuré et importation des informations nécessaires depuis data.json pour utiliser dans notre Composant Card importé ici

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,5 +1,22 @@
-const gallery = () => {
-  return;
-};
+import data from "../data/data.json";
+import Card from "../components/Card";
+import "../styles/Gallery.css";
 
-export default gallery;
+function Gallery() {
+  return (
+    <div className="gallery">
+      {data.map((data) => {
+        return (
+          <Card
+            key={data.id}
+            id={data.id}
+            title={data.title}
+            cover={data.cover}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default Gallery();
