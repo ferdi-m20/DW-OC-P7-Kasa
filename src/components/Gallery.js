c2380d62582e61e1cf5ef220a70cf88dbb291550
Ferdinand BLANCHART <ferdinand_m20@yahoo.fr>
2023-04-15 01:24:09 +0200

    nom du import data changé en apartments dans Gallery.js et Apartment.js puis une fonction ApartmentNotFound pour gérer l'erreur 404 dans Apartment.js

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,17 +1,17 @@
-import data from "../data/data.json";
+import apartments from "../data/data.json";
 import Card from "../components/Card";
 import "../styles/Gallery.css";
 
 function Gallery() {
   return (
     <div className="gallery">
-      {data.map((data) => {
+      {apartments.map((apartment) => {
         return (
           <Card
-            key={data.id}
-            id={data.id}
-            title={data.title}
-            cover={data.cover}
+            key={apartment.id}
+            id={apartment.id}
+            title={apartment.title}
+            cover={apartment.cover}
           />
         );
       })}
--- src/pages/Apartment.js
+++ src/pages/Apartment.js
@@ -1,8 +1,24 @@
+import { useParams } from "react-router-dom";
+import apartments from "../data/data.json";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import NotFound from "./NotFound";
 import "../styles/Apartment.css";
 
-function Apartment() {
+export default function ApartmentNotFound() {
+  const apartmentId = useParams("id").id;
+  const singleApartmentDetail = apartments.filter(
+    (apartment) => apartment.id === apartmentId
+  );
+  console.log(singleApartmentDetail);
+  if (singleApartmentDetail.length === 0) {
+    return <NotFound />;
+  } else {
+    return <Apartment />;
+  }
+}
+
+export function Apartment() {
   return (
     <div>
       <Header />
@@ -10,5 +26,3 @@ function Apartment() {
     </div>
   );
 }
-
-export default Apartment;
