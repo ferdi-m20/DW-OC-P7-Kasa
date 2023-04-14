84988a7fe39be32b6c1b20a0c23db2451e73e3d9
Ferdinand BLANCHART <ferdinand_m20@yahoo.fr>
2023-04-14 23:44:54 +0200

    Ajout de la route apartment dans Routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-// import Apartment from "./pages/Apartment";
+import Apartment from "./pages/Apartment";
 import NotFound from "./pages/NotFound";
 
 const myRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      {/* <Route path="/apartment/:id" element={<Apartment />} /> */}
+      <Route path="/apartment/:id" element={<Apartment />} />
       <Route path="/about" element={<About />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
