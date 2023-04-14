8c32a27467a8ec52ca1f132fad417c9f07cffb24
Ferdinand BLANCHART <ferdinand_m20@yahoo.fr>
2023-04-14 21:15:12 +0200

    Composant Banner crée

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,6 +2,27 @@ import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import "../styles/Banner.css";
 
-function Banner() {}
+function Banner() {
+  const [aboutPage, setAboutPage] = useState(false);
+
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === "/about") {
+      setAboutPage(true);
+    }
+  }, []);
+
+  return (
+    <section className={aboutPage ? "about-banner" : "home-banner"}>
+      {!aboutPage && (
+        <div className="home-banner-title">
+          <div>Chez vous,</div>
+          <div>partout et ailleurs</div>
+        </div>
+      )}
+    </section>
+  );
+}
 
 export default Banner;
