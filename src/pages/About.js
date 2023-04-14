203f5260215e36a5ebebee15c3b3a1aaa69ef638
Ferdinand BLANCHART <ferdinand_m20@yahoo.fr>
2023-04-14 22:30:46 +0200

    Importation des des composants dans About.js et crée la fonction About()

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+import Banner from "../components/Banner";
+import Footer from "../components/Footer";
+
+function About() {
+  return (
+    <div className="About">
+      <Header />
+      <Banner />
+      <Footer />
+    </div>
+  );
+}
+
+export default About;
