f5a86a4965b99f160c755c9ba71dd567cb0f479c
Ferdinand BLANCHART <ferdinand_m20@yahoo.fr>
2023-04-14 23:52:13 +0200

    Page Apartment.js configuré avec Header et Footer Composants

---
+++ src/pages/Apartment.js
@@ -0,0 +1,14 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import "../styles/Apartment.css";
+
+function Apartment() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default Apartment;
