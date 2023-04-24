4a079449d9bca0a1d15a4cdf83cff460e3724e75
Ferdinand BLANCHART <ferdinand_m20@yahoo.fr>
2023-04-24 13:52:04 +0200

    Composant Collapse crée et fonctionnel

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -1,5 +1,35 @@
-function Collpase() {
-  return;
+import { useState } from "react";
+import arrow from "../assets/arrow_down.svg";
+import "../styles/Collapse.css";
+
+function Collpase({ title, content }) {
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <>
+      <div className="collapse">
+        <h3 className="collapse-title" onClick={() => setToggle(!toggle)}>
+          {title}
+          <img
+            className={
+              toggle ? "arrow-effect arrow-up" : "arrow-effect arrow-down"
+            }
+            src={arrow}
+            alt="flèche vers le bas ou le haut"
+          />
+        </h3>
+        <div
+          className={toggle ? "collapse-content" : "collapse-content-hidden"}
+        >
+          {Array.isArray(content)
+            ? content.map((item, index) => {
+                return <p key={index}>{item}</p>;
+              })
+            : content}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Collpase;
